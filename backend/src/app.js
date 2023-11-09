84fe069973dfacd802f0cd8ea72198e2df5d0891
YanBastos <yanmoraesbastos@hotmail.com>
2023-11-09 16:17:20 -0300

    add cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,13 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable no-unused-vars */
+const cors = require('cors');
 const express = require('express');
 const router = require('./router.js');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(router);
 
 module.exports = app;
